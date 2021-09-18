07f0e59b08550db58e1c0833ffa57e48b72dfd42
antoniomrrds <arqueiroverd@gmail.com>
2021-09-18 00:44:00 -0300

    Projeto de legendas terminado 18-09-2021

--- curso_js_funcional/Projeto de legendas/index.js
+++ curso_js_funcional/Projeto de legendas/index.js
@@ -4,11 +4,27 @@ const fn = require('./funcoes')
 const caminho = path.join(__dirname,'legendas')
 
 
+const simbolos = [
+    '.', '?', '-', ',', '"', '♪',
+    '_', '<i>', '</i>', '\r', '[', ']',
+    '(', ')'
+]
+
 
 
 fn.lerDiretorio(caminho)
-.then(arquivos => fn.filterExtesion(arquivos,'.txt'))
-.then(console.log)
-// .then()
-// .catch(err => console.log(`Erro: ${err} !`))
-// console.log(arquivos)
+    .then(fn.elementosTerminadosCom('.srt'))
+    .then(fn.lerArquivos)
+    .then(fn.mesclarElementos)
+    .then(fn.separarTextoPor('\n'))
+    .then(fn.removerElementosSeVazio)
+    .then(fn.removerElementosSeIncluir('-->'))
+    .then(fn.removerElementosSeApenasNumero)
+    .then(fn.removerSimbolos(simbolos))
+    .then(fn.mesclarElementos)
+    .then(fn.separarTextoPor(' '))
+    .then(fn.removerElementosSeVazio)
+    .then(fn.removerElementosSeApenasNumero)
+    .then(fn.agruparElementos)
+    .then(fn.ordernarPorAtribNumerico('qtde', 'desc'))
+    .then(console.log)
